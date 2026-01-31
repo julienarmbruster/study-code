0982735e3dd7240fb68e50591f645b71ef7f4d62
Julien Armbruster <Julien.armbruster06@gmail.com>
2026-01-31 21:03:08 +0100

    schachbrett 1

---
+++ courses/coding-in-C/Lab_2/lab_2mysolution1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    unsigned char r, s;
+    for(r=8;r>0;r--){
+        for(s=65;s<=72;s++){
+            printf("%c%-2.d",s ,r);
+
+        }
+        printf("\n");
+
+    }
+
+
+    return 0;
+}
